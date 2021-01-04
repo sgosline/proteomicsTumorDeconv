c1c591afda383d4078aac1fd33d93bd8fc114207
Anna Calinawan <anna.pamela@gmail.com>
2021-01-04 17:55:11 -0500

    refactor main into separate functions for more straightforward unit testing

--- mRNAData/mRNADataSetsCLI.py
+++ mRNAData/mRNADataSetsCLI.py
@@ -10,34 +10,37 @@ def parse_cancer_type():
     parser = argparse.ArgumentParser()
     parser.add_argument('--cancerType', dest='type',
                         help='Cancer type to be collected')
-    return parser.parse_args()
+    return parser.parse_args().type.lower()
 
-def main(cancer_type):
-    opts = cancer_type
-
-    if opts.type.lower() == 'brca':
+def get_cptac_df(cancer_type):
+    if cancer_type == 'brca':
         dat = cptac.Brca()
-    elif opts.type.lower() == 'ccrcc':
+    elif cancer_type == 'ccrcc':
         dat = cptac.Ccrcc()
-    elif opts.type.lower() == 'coad':
+    elif cancer_type == 'coad':
         dat = cptac.Colon()
-    elif opts.type.lower() == 'ovca':
+    elif cancer_type == 'ovca':
         dat = cptac.Ovarian()
-    elif opts.type.lower() == 'endometrial':
+    elif cancer_type == 'endometrial':
         dat = cptac.Endometrial()
-    elif opts.type.lower() == 'gbm':
+    elif cancer_type == 'gbm':
         dat = cptac.Gbm()
-    elif opts.type.lower() == 'hnscc':
+    elif cancer_type == 'hnscc':
         dat = cptac.Hnscc()
-    elif opts.type.lower() == 'lscc':
+    elif cancer_type == 'lscc':
         dat = cptac.Lscc()
-    elif opts.type.lower() == 'luad':
+    elif cancer_type == 'luad':
         dat = cptac.Luad()
     else:
         exit()
     df = dat.get_transcriptomics()
-    df.transpose().to_csv(path_or_buf="file.tsv", sep='\t')
+
+    return df
+
+def write_file(cancer_type):
+    cptac_df = get_cptac_df(cancer_type)
+    cptac_df.transpose().to_csv(path_or_buf="file.tsv", sep='\t')
 
 
 if __name__ == '__main__':
-    main(cancer_type=parse_cancer_type())
+    write_file(cancer_type=parse_cancer_type())
